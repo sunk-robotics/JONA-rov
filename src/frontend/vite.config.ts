3e3c58611307ff2fd47c6184aca8bcd42977961f
Noah Gibson <thebottemofthebarrel@gmail.com>
2024-02-07 14:03:15 -0500

    tricked ssl certificate

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+const fs = require('fs')
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -15,5 +16,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server: { port: 3000 }
+  server: { 
+    port: 3000,
+    https: {
+      key: fs.readFileSync('./certs/example.com+5-key.pem'),
+      cert: fs.readFileSync('./certs/example.com+5.pem'),
+    },
+  }
 })
