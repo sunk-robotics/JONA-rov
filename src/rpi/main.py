1bc8cf47664c0ee9b105eb84e007bd8ff1721c7f
Noah Gibson <thebottemofthebarrel@gmail.com>
2024-02-23 17:01:38 -0500

    added stabilization breaker

--- src/rpi/main.py
+++ src/rpi/main.py
@@ -7,6 +7,9 @@ from motors import Motors
 from ms5837 import MS5837_02BA
 import time
 import websockets
+from pid import PID
+
+DESTABLE_THRESH = 0.5 # when depth stabiliziation is active, how much the joystick needs to push to stop stabilizing
 
 
 # websocket server
@@ -82,44 +85,6 @@ class WSServer:
         elif client_type == "web_client_camera":
             await cls.web_client_camera_handler(websocket, path)
 
-
-# used to adjust the motor velocities to keep the ROV at a constant position
-class PID:
-    last_time = None
-    last_error = None
-
-    def __init__(self, set_point=0, proportional_gain=0, integral_gain=0,
-                 derivative_gain=0):
-        self.set_point = set_point
-        self.proportional_gain = proportional_gain
-        self.integral_gain = integral_gain
-        self.derivative_gain = derivative_gain
-
-        self.integral = 0
-        self.last_time = time.time()
-        self.last_error = set_point
-
-    # takes in the acceleration as the process value
-    def compute(self, process_value):
-        current_time = time.time()
-        d_time = time.time() - self.last_time
-        self.last_time = current_time
-
-        # difference between the target and measured acceleration
-        error = self.set_point - process_value
-        #  print(f"Set point: {self.set_point}")
-        #  print(f"Process value: {process_value}")
-        #  print(f"Error: {error}")
-        # compute the integral ∫e(t) dt
-        self.integral += error * d_time
-        # compute the derivative
-        d_error = (error - self.last_error) / d_time
-        self.last_error = error
-
-        # add the P, I, and the D together
-        output = (self.proportional_gain * error + self.integral_gain
-                  * self.integral + self.derivative_gain * d_error)
-        return output
 
 
 async def main_server():
@@ -216,8 +181,10 @@ async def main_server():
 
         # set the z velocity according to the vertical PID controller based on
         # current depth
-        if vertical_anchor and depth_sensor is not None:
+        if vertical_anchor and depth_sensor is not None and abs(joystick_data["left_stick"][1]) > DESTABLE_THRESH:
             z_velocity = vertical_pid.compute(depth_sensor.depth())
+
+        
 
         # set the yaw velocity according to the yaw PID controller based on
         # current yaw angle
