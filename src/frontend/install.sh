525d13297e5004b370ae2f1667194a425d511e06
XiXinping <81447868+XiXinping@users.noreply.github.com>
2024-04-16 16:34:00 +0000

    Update install.sh

--- src/frontend/install.sh
+++ src/frontend/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo rm -rf /var/jona-server
+sudo rm -rf /var/jona-server
+echo "Deleted old frontend code"
 sudo cp -r /home/sunk/JONA-rov/src/frontend /var/jona-server
 sudo chown -R sunk:sunk /var/jona-server
+echo "Installed new frontend code"
