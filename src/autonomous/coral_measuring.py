398b17b5be023f82c7089ef95338142d1d592ee8
XiXinping <redbinary@protonmail.com>
2024-05-03 15:44:50 -0400

    Coral measuring stuff and such

--- src/autonomous/coral_measuring.py
+++ src/autonomous/coral_measuring.py
@@ -3,7 +3,22 @@ import numpy as np
 
 
 def main():
-    pass
+    img = cv2.imread("coral_images/coral_measuring1.png")
+
+    img_blur = cv2.GaussianBlur(img, (9, 9), 0)
+
+    lower_orange = np.array([0, 0, 0])
+    upper_orange = np.array([0, 0, 0])
+    gray_img = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
+    ok, thresh = cv2.threshold(gray_img, 1, 255, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours(
+        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    )
+    cv2.drawContours(img, contours, -1, (0, 255, 0))
+    cv2.imshow("Contours!", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
