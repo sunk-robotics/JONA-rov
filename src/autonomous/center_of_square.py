970d68064b4c85c23c0fae458ee9500f79b36987
XiXinping <redbinary@protonmail.com>
2024-05-06 14:45:57 -0400

    Improving image segmentation

--- src/autonomous/center_of_square.py
+++ src/autonomous/center_of_square.py
@@ -2,51 +2,57 @@ import cv2
 import numpy as np
 from scipy.interpolate import splprep, splev
 
+
 def smooth_contour(contour: np.ndarray) -> np.ndarray:
     x, y = contour.T
     x = x.tolist()[0]
     y = y.tolist()[0]
-    tck, u = splprep([x,y], u=None, s=1.0, per=1)
+    tck, u = splprep([x, y], u=None, s=1.0, per=1)
     u_new = np.linspace(u.min(), u.max(), 100)
     x_new, y_new = splev(u_new, tck, der=0)
-    res_array = [[[int(i[0]), int(i[1])]] for i in zip(x_new,y_new)]
+    res_array = [[[int(i[0]), int(i[1])]] for i in zip(x_new, y_new)]
     return np.asarray(res_array, dtype=np.int32)
 
 
 # Remove the horkfook from the image, which often interferes with the image
 # segmentation algorithm. Image must be HSV (Hue, Saturation, Value).
 def filter_out_hork(img: np.ndarray) -> np.ndarray:
-    lower_orange = np.array([10, 10, 10])
-    upper_orange = np.array([30, 255, 255])
+    lower_orange = np.array([13, 10, 10])
+    upper_orange = np.array([35, 255, 255])
     orange_mask = cv2.inRange(img, lower_orange, upper_orange)
     # turn all parts of the image that aren't orange into black
     orange_img = cv2.bitwise_and(img, img, mask=orange_mask)
     gray_img = cv2.split(orange_img)[2]
-    cv2.imshow("Gray", gray_img)
     ok, thresh = cv2.threshold(gray_img, 1, 255, cv2.THRESH_BINARY)
-    cv2.imshow("Thresh", thresh)
+    #  cv2.imshow("Thresh", thresh)
     kernel = np.ones((5, 5), np.uint8)
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-    cv2.imshow("Thresh2", thresh)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
+    #  cv2.imshow("Thresh2", thresh)
 
     # find the contours of the two hooks
     contours, hierarchy = cv2.findContours(
         thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
-    hook_contours = sorted(contours, key=cv2.contourArea)[0:2]
-    cv2.drawContours
-dfsdfzszdfsfdfrange sdfsdfsdfsdfsdfdsdfcsdsf   dsfsdffexc xcc v    fdsfsffdsfasdxfds    smoothened_contours = [smooth_contour(hook_contours[0]), smooth_contour(hook_contours[1])]
-    
+    hook_contours = sorted(contours, key=cv2.contourArea, reverse=True)[0:2]
+    if len(hook_contours) < 2:
+        print("Unable to filter out hork!")
+        return img
+    smoothened_contours = [
+        smooth_contour(hook_contours[0]),
+        smooth_contour(hook_contours[1]),
+    ]
+
     mask = np.zeros_like(thresh)
     cv2.drawContours(mask, smoothened_contours, -1, 255, -1)
     # dilate the mask to remove the pixels around the edges of the hooks that
     # like to cause problems
     kernel = np.ones((5, 5), np.uint8)
-    mask = cv2.dilate(mask, kernel, iterations=1)
+    mask = cv2.dilate(mask, kernel, iterations=2)
+    cv2.imshow("Mask", mask)
     cv2.bitwise_not(mask, mask)
 
     return cv2.bitwise_and(img, img, mask=mask)
-    
+
 
 # find the x and y coordinates of the center of a red object in the image
 def center_of_red(img: np.ndarray) -> (int, int):
@@ -74,7 +80,7 @@ def center_of_red(img: np.ndarray) -> (int, int):
     # converting to HSV (Hue, Saturation, Value) makes it easier to identify a range
     # of possibcv2.imshow('mask', mask)le red values
     img_hsv = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2HSV)
-    horkless_img = filter_out_hork(img_hsv)
+    #  horkless_img = filter_out_hork(img_hsv)
 
     #  (h, s, v) = cv2.split(img_hsv)
     #  s = s * 2
@@ -97,18 +103,21 @@ def center_of_red(img: np.ndarray) -> (int, int):
     # away from the square, the grayer/browner the square becomes, so gradually
     # increase the range of colors until the square can be found
     for i in range(0, 7):
+        filter_img = filter_out_hork(img_hsv) if i > 3 else img_hsv
         lower_red = np.array([0, 10, 10])
         upper_red = np.array([i * 5 + 10, 255, 150])
-        red_mask = cv2.inRange(horkless_img, lower_red, upper_red)
+        red_mask = cv2.inRange(filter_img, lower_red, upper_red)
 
-        cv2.imshow("Horkless Image", cv2.cvtColor(horkless_img, cv2.COLOR_HSV2BGR))
+        cv2.imshow("Horkless Image", cv2.cvtColor(filter_img, cv2.COLOR_HSV2BGR))
         # turn all parts of the image that aren't red into black
-        red_img = cv2.bitwise_and(horkless_img, horkless_img, mask=red_mask)
+        red_img = cv2.bitwise_and(filter_img, filter_img, mask=red_mask)
         cv2.imshow("Red Image", cv2.cvtColor(red_img, cv2.COLOR_HSV2BGR))
 
         # turn the image into a binary (black and white) image, where the white parts
         # represent anything red, and the black parts represent anything not red
         gray_img = cv2.split(red_img)[2]
+        #  gray_img = cv2.equalizeHist(gray_img)
+        #  cv2.imshow("Equalized Image", cv2.cvtColor(gray_img, cv2.COLOR_GRAY2BGR))
         ok, thresh = cv2.threshold(gray_img, 1, 255, cv2.THRESH_BINARY)
 
         contours, hierarchy = cv2.findContours(
@@ -120,7 +129,9 @@ def center_of_red(img: np.ndarray) -> (int, int):
             and cv2.contourArea(max(contours, key=cv2.contourArea)) > 50
         ):
             largest_contour = max(contours, key=cv2.contourArea)
-            print(f"Ratio of Perimeter to Area: {cv2.arcLength(largest_contour, True) / cv2.contourArea(largest_contour)}")
+            print(
+                f"Ratio of Perimeter to Area: {cv2.arcLength(largest_contour, True) / cv2.contourArea(largest_contour)}"
+            )
             break
 
     if largest_contour is None:
@@ -128,7 +139,7 @@ def center_of_red(img: np.ndarray) -> (int, int):
 
     moment = cv2.moments(largest_contour)
     if moment["m00"] == 0:
-        print('shit')
+        print("shit")
         return None, None
 
     x_coord = int(moment["m10"] / moment["m00"])
@@ -140,8 +151,8 @@ def main():
     #  vc = cv2.VideoCapture(0)
 
     # problem images: 5, 8, 9, 12
-    img = cv2.imread("coral_images/red_square1.png")
-    
+    img = cv2.imread("coral_images/red_square9.png")
+
     x_coord, y_coord = center_of_red(img)
     if x_coord is not None and y_coord is not None:
         cv2.circle(img, (x_coord, y_coord), 5, (255, 255, 255), -1)
