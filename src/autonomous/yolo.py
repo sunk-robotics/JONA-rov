1a094f93becb275d02391f150b4c03c8db075d87
XiXinping <redbinary@protonmail.com>
2024-05-23 18:24:22 -0400

    Imported trained weights

--- src/autonomous/yolo.py
+++ src/autonomous/yolo.py
@@ -1,18 +1,23 @@
 import cv2
 from ultralytics import YOLO
+import glob
 
 # Load a YOLOv8n PyTorch model
 #  model = YOLO("yolov8x-seg.pt")
-model = YOLO("yolov9c-seg.pt")
+#  model = YOLO("yolov9c-seg.pt")
+model = YOLO("best.pt")
 
 #  Export the model to NCNN format
-model.export(format="ncnn")  # creates 'yolov8n_ncnn_model'
+#  model.export(format="ncnn")  # creates 'yolov8n_ncnn_model'
 
 # Load the exported NCNN model
-ncnn_model = YOLO("yolov9c-seg_ncnn_model")
+#  ncnn_model = YOLO("yolov9c-seg_ncnn_model")
 
-results = ncnn_model("coral_images/red_square1.png")
-annotated_frame = results[0].plot()
-cv2.imshow("Object Detection", annotated_frame)
-cv2.waitKey(0)
+
+for filepath in glob.iglob("coral_dataset/valid/images/*.jpg"):
+    results = model(filepath)
+    annotated_frame = results[0].plot()
+    cv2.imshow("Object Detection", annotated_frame)
+    cv2.waitKey(1000)
+
 cv2.destroyAllWindows()
