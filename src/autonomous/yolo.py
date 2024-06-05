d08c434457e9e2e22b19c52ad7bd48f35eb2b22b
XiXinping <redbinary@protonmail.com>
2024-06-05 14:16:07 -0400

    Improved performance of square detection algorithm

--- src/autonomous/yolo.py
+++ src/autonomous/yolo.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import time
 from ultralytics import YOLO
 import glob
@@ -8,7 +9,22 @@ model = YOLO("best.pt")
 
 for filepath in glob.iglob("coral_dataset/train/new_images/*.jpg"):
     img = cv2.imread(filepath)
-    results = model.predict(source=img, save=False)
+    img_width = img.shape[1]
+    img_height = img.shape[0]
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    mask = np.zeros(img.shape[:2], dtype="uint8")
+    cv2.rectangle(
+        mask,
+        (0, 0),
+        (int(img_width * (9 / 10)), img_height),
+        255,
+        -1,
+    )
+    cropped_img = cv2.bitwise_and(gray, gray, mask=mask)
+    cv2.imwrite("ooga.jpg", cropped_img)
+    cropped_img = cv2.imread("ooga.jpg")
+
+    results = model.predict(source=cropped_img, save=False, imgsz=(448, 256))
     if len(results) > 0 and len(results[0]) > 0:
         x1, y1, x2, y2 = [round(tensor.item()) for tensor in results[0].boxes.xyxy[0]]
         print(x1, y2, x2, y2)
@@ -26,8 +42,9 @@ for filepath in glob.iglob("coral_dataset/train/new_images/*.jpg"):
             (255, 255, 255),
             2,
         )
-    #  cv2.imshow("Center of Square", img)
-    cv2.imshow("Object Detection", annotated_frame)
+        cv2.imshow("Object Detection", annotated_frame)
+    else:
+        cv2.imshow("Object Detection", img)
     cv2.waitKey(0)
 
 cv2.destroyAllWindows()
