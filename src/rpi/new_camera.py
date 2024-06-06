b8837078b5621d906c1864871f3ebbb42c9ea2a8
Sunk Robotics <sunk@sunkrobotics.com>
2024-06-06 17:01:07 -0400

    New camera code

---
+++ src/rpi/new_camera.py
@@ -0,0 +1,85 @@
+# TO-DO: THERE'S A MEMORY LEAK
+#!/usr/bin/python
+from picamera2 import Picamera2
+from picamera2.outputs import FileOutput
+from picamera2.encoders import MJPEGEncoder
+import io
+import asyncio
+from libcamera import controls, Rectangle
+from websockets import broadcast, serve, ConnectionClosed, ConnectionClosedOK
+
+
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self, new_frame_event):
+        self.frame = None
+        self.new_frame_event = new_frame_event
+
+    def write(self, buf):
+        self.frame = buf
+        self.new_frame_event.set()
+        self.new_frame_event.clear()
+
+
+class WSServer:
+    new_frame_event = asyncio.Event()
+    output = StreamingOutput(new_frame_event)
+    clients = set()
+
+    @classmethod
+    async def listen_for_frame(cls):
+        while True:
+            if cls.output.frame is None:
+                await asyncio.sleep(0.001)
+                continue
+            broadcast(cls.clients, bytearray(cls.output.frame))
+            # for websocket in cls.clients:
+            #     try:
+            #         await websocket.send(bytearray(cls.output.frame))
+            #     except ConnectionClosed:
+            #        print("Unable to send frame! Connection closed!")
+            cls.output.frame = None
+            await asyncio.sleep(0.001)
+
+    @classmethod
+    async def handler(cls, websocket, path):
+        print("Client connected!")
+        cls.clients.add(websocket)
+        try:
+            await websocket.wait_closed()
+            cls.clients.remove(websocket)
+            print("Client disconnected!")
+        except ConnectionClosed:
+            print("Client disconnected!")
+            cls.clients.remove(websocket)
+
+
+def main():
+    picam2 = Picamera2()
+    print(picam2.sensor_modes)
+    config = picam2.create_video_configuration(
+        main={"size": (854, 480)}, raw={"size": (1920, 1080)}
+    )
+    # config = picam2.create_video_configuration(main={"size": (1280, 720)})
+    picam2.configure(config)
+    picam2.set_controls(
+        {
+            "AfMode": controls.AfModeEnum.Continuous,
+            "AeEnable": True,
+            "AwbEnable": True,
+            "AwbMode": controls.AwbModeEnum.Cloudy,
+            "Saturation": 1.0,
+            "Contrast": 1.0,
+        }
+    )
+    picam2.start_recording(MJPEGEncoder(25_000_000), FileOutput(WSServer.output))
+
+    loop = asyncio.get_event_loop()
+    ws_server = serve(WSServer.handler, "0.0.0.0", 3000, ping_interval=None)
+    print("Server started!")
+    asyncio.ensure_future(ws_server)
+    asyncio.ensure_future(WSServer.listen_for_frame())
+    loop.run_forever()
+
+
+if __name__ == "__main__":
+    main()
